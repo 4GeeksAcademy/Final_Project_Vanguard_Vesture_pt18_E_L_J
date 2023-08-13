1828a24eed280fafd0c7444422b9685eb53853b6
Luis  Vela <luismvl114@gmail.com>
2023-08-13 13:02:39 -0500

    paypal funcionando

--- src/front/js/component/PaymentComponent.jsx
+++ src/front/js/component/PaymentComponent.jsx
@@ -1,83 +1,65 @@
-import React , {useContext} from 'react'
-import { PayPalButtons } from "@paypal/react-paypal-js";
+import React, { useContext } from 'react'
+import { PayPalButtons } from '@paypal/react-paypal-js'
 import { Context } from '../store/appContext.js'
 
+const PaymentComponent = () => {
+  const { store, actions } = useContext(Context)
 
+  const stylePay = {
+    layout: 'vertical',
+    color: 'black',
+    label: 'buynow',
+    shape: 'pill',
+  }
 
-const PaymentComponent = ({cost}) => {
+  const createOrder = async () => {
+    const backendUrl =
+      process.env.BACKEND_URL + `api/create-paypal-order/`
+    return await fetch(backendUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        cart: store.shopping_cart,
+        amount: actions.getTotalCart(),
+      }),
+    })
+      .then((response) => response.json())
+      .then((order) => order.id)
+  }
+  const onApprove = async (data) => {
+    // Order is captured on the server and the response is returned to the browser
+    const backendUrl =
+      process.env.BACKEND_URL + `api/capture-paypal-order`
+    return await fetch(backendUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        orderID: data.orderID,
+      }),
+    })
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log('Successfully payment:', responseData)
+      })
+      .catch((error) => {
+        console.error('Error capturing payment:', error)
+      })
+  }
 
-    const {  store } = useContext(Context)
-
-    const stylePay = {"layout":"vertical",
-"color": "black",
-"label":"buynow",
-"shape":"pill"
-      };
-
-    const generateUniqueOrderID = () => {
-        const currentDate = new Date();
-        const randomSuffix = Math.floor(Math.random() * 1000); // Genera un número aleatorio entre 0 y 999
-        const orderID = `ORDER-${currentDate.getTime()}-${randomSuffix}`;
-        return orderID;
-    }
-
-    const orderID = generateUniqueOrderID()
-
-    const createOrder = async () => {
-        // Order is created on the server and the order id is returned
-        const user_id = store.user.id
-        const backendUrl = process.env.BACKEND_URL + `api/payment/create-paypal-order/${user_id}`;
-        return await fetch(backendUrl, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            // use the "body" param to optionally pass additional order information
-            // like product skus and quantities 
-            body: JSON.stringify({
-                    orderID: orderID,
-                    articles: ['art1'],
-                    cost: {cost}
-            }),
-        })
-            .then((response) => response.json())
-            .then((order) => order.id);
-    };
-    const onApprove = async (data) => {
-        // Order is captured on the server and the response is returned to the browser
-        const user_id = store.user.id
-        const backendUrl = process.env.BACKEND_URL + `api/payment/capture-paypal-order/${user_id}`;
-        return await fetch(backendUrl, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                orderID: data.orderID
-            })
-        })
-        .then((response) => response.json())
-        .then((responseData) => {
-            console.log('Successfully payment:', responseData);
-        })
-        .catch((error) => {
-            console.error('Error capturing payment:', error);
-        });
-    };
-
-    return (
-        <PayPalButtons
-            createOrder={(data, actions) => createOrder(data, actions)}
-            onApprove={(data, actions) => onApprove(data, actions)}
-            style={stylePay}
-            disabled={undefined}
-            forceReRender={[stylePay]}
-            fundingSource={undefined}
-            
-        />
-    );
+  return (
+    <PayPalButtons
+      createOrder={(data, actions) => createOrder(data, actions)}
+      onApprove={(data, actions) => onApprove(data, actions)}
+      style={stylePay}
+      disabled={undefined}
+      forceReRender={[stylePay]}
+      fundingSource={undefined}
+    />
+  )
 }
-
-
 
 export default PaymentComponent
