14944173371fcae3556f741838f1a8e24740fabc
Luis  Vela <luismvl114@gmail.com>
2023-08-21 19:26:43 -0500

    fix: la imágenes ahora se guardan con el url en HTTPS

--- webpack.common.js
+++ webpack.common.js
@@ -40,7 +40,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-        favicon: 'favicon.png',
+        favicon: 'favicon.ico',
         template: 'template.html'
     }),
     new Dotenv({ safe: true, systemvars: true })
